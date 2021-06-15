46abbdebeddb4cfa6240a8c3517349ed92920f3a
Doug Erkkila <derkkila@users.noreply.github.com>
2021-06-15 12:31:25 -0400

    missing files for actions

---
+++ github_app_for_splunk/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="TA_splunk_ghe_audit_log_monitoring",
+    version="X.Y.Z",
+    url="https://github.com/splunk/TA_splunk_ghe_audit_log_monitoring",
+    author="Bassem Dghaidi and Doug Erkkila",
+    author_email="link-@github.com",
+    description="Splunk modular input to pull audit log events from GitHub Enterprise",
+    packages=['TA_splunk_ghe_audit_log_monitoring'],
+    license="MIT"
+)
