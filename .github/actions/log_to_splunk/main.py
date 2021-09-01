042e67d3f18b3508ec59d3b6383f19d43b817f3a
Doug Erkkila <derkkila@users.noreply.github.com>
2021-09-01 16:30:10 -0400

    Update main.py

--- .github/actions/log_to_splunk/main.py
+++ .github/actions/log_to_splunk/main.py
@@ -21,7 +21,7 @@ def main():
     headers = {"Authorization": "Splunk "+SPLUNK_HEC_TOKEN, "Accept": "application/vnd.github.v3+json"}
     host="$HOSTNAME"
 
-    url = "{url}/repos/{repo}/actions/runs/{run_id}/logs".format(url=GITHUB_API_URL,repo=GITHUB_REPOSITORY,run_id=GITHUB_RUN_ID)
+    url = "{url}/repos/{repo}/actions/runs/{run_id}/logs".format(url=GITHUB_API_URL,repo=GITHUB_REPOSITORY,run_id=GITHUB_WORKFLOWID)
     print(url)
 
     try:
