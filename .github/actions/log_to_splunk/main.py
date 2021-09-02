e7f1e15cd33afe61a628c6e71d5c8176cb277a0b
Doug Erkkila <derkkila@users.noreply.github.com>
2021-09-02 13:47:36 -0400

    Update main.py

--- .github/actions/log_to_splunk/main.py
+++ .github/actions/log_to_splunk/main.py
@@ -65,11 +65,11 @@ def main():
         Lines = logfile.readlines()
         for line in Lines:
             count+=1
-            timestamp = re.search(\"\d{4}-\d{2}-\d{2}T\d+:\d+:\d+.\d+Z\",line.strip())
-            timestamp = re.sub(\"\dZ\",\"\",timestamp.group())
-            timestamp = datetime.strptime(timestamp,\"%Y-%m-%dT%H:%M:%S.%f\")
+            timestamp = re.search("\d{4}-\d{2}-\d{2}T\d+:\d+:\d+.\d+Z",line.strip())
+            timestamp = re.sub("\dZ","",timestamp.group())
+            timestamp = datetime.strptime(timestamp,"%Y-%m-%dT%H:%M:%S.%f")
             timestamp = (timestamp - datetime(1970,1,1)).total_seconds()
-            x = re.sub(\"\d{4}-\d{2}-\d{2}T\d+:\d+:\d+.\d+Z\",\"\",line.strip())
+            x = re.sub("\d{4}-\d{2}-\d{2}T\d+:\d+:\d+.\d+Z","",line.strip())
             x=x.strip()
             fields = {'lineNumber':count,'workflowID':GITHUB_WORKFLOWID}
             if x:
