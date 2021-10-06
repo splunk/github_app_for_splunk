3cbd24d930595f6579f52e8647bbb11e8d22e891
Doug Erkkila <derkkila@users.noreply.github.com>
2021-10-06 16:19:08 -0400

    Update main.py

--- .github/actions/log_to_splunk/main.py
+++ .github/actions/log_to_splunk/main.py
@@ -54,14 +54,12 @@ def main():
 
     summary = json.loads(x.text)
 
-    for element in summary:
-        element.pop('repository', None)
+    summary.pop('repository')
 
     summary["repository"]=summary["head_repository"]["name"]
     summary["repository_full"]=summary["head_repository"]["full_name"]
 
-    for element in summary:
-        element.pop('head_repository', None)
+    summary.pop('head_repository')
 
 
     url = "{url}/repos/{repo}/actions/runs/{run_id}/logs".format(url=GITHUB_API_URL,repo=GITHUB_REPOSITORY,run_id=GITHUB_WORKFLOWID)
