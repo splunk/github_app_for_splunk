840a1254e5d1c931f2fb363ea6ba04151e4b461d
Doug Erkkila <derkkila@users.noreply.github.com>
2022-08-19 14:18:50 -0400

    First API
    First API call test

--- .github/actions/appinspect_publish/index.js
+++ .github/actions/appinspect_publish/index.js
@@ -10,6 +10,13 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  // Call an API endpoint with the payload
+  const response = await fetch('https://splunkbase.splunk.com/api/v1/app/${AppID}/new_release/', {
+      method: 'POST',
+      body: payload
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
