4d356dca308eabcde16ef71dc529f575d2d2129e
Doug Erkkila <derkkila@users.noreply.github.com>
2022-09-19 16:10:44 -0400

    Remove old test files
    Update path

--- .github/actions/appinspect_publish/publish.py
+++ .github/actions/appinspect_publish/publish.py
@@ -3,8 +3,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 APP_ID= os.environ['INPUT_APP_ID']
-filename = os.environ['INPUT_APP_FILE']
-filepath = os.path.join(os.environ['GITHUB_WORKSPACE'], filename)
+filepath = os.environ['INPUT_APP_FILE']
 SPLUNK_USERNAME = os.environ['INPUT_SPLUNK_USERNAME']
 SPLUNK_PASSWORD = os.environ['INPUT_SPLUNK_PASSWORD']
 SPLUNK_VERSION = os.environ['INPUT_SPLUNK_VERSION']
@@ -29,9 +28,8 @@ print(response.status_code)
 print(response.text)
 
 # if status code is not 200, print the response text
-if response.status_code != 200:    
+if response.status_code != 200:
     response.raise_for_status()
     exit(response.status_code)
 else:
     exit(0)
-
